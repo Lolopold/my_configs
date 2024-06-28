510dbcc0a71638363f74c8d7aa3b69784c1c18e4
Lolopold <lolopold.socials@tutanota.com>
2024-06-29 00:36:36 +0200

    installing nvim without traces in the current directory

--- nvim/nvim_conf.sh
+++ nvim/nvim_conf.sh
@@ -3,4 +3,7 @@ wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 nvim=./squashfs-root/usr/bin/nvim
-git clone https://github.com/NvChad/starter ~/.config/nvim && $nvim
+git clone https://github.com/NvChad/starter ~/.config/nvim
+rm nvim.appimage
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
