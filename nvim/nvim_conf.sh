a2be72f170da920e9fbba9a41a14cbcafff12be8
Lolopold <lolopold.socials@tutanota.com>
2024-06-29 12:39:47 +0200

    added features for removing nvim and choice to install nvchad

--- nvim/nvim_conf.sh
+++ nvim/nvim_conf.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+echo "Remove Neovim, y/n?"
+read nvim
 ## Get sudo access
 sudo -
+if [[ "$nvim" = "y" ]]; then
+  sudo apt remove nvim
+  sudo rm -fr /squashfs-root/
+  sudo rm /usr/bin/nvim
+  exit 0
+fi
+echo "Install NvChad, y/n?"
+read chad
 ## Download latest neovim version, make it executable and extract the files
 wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 ## remove prior neovim configurations
+if [[ "$chad" = "y" ]]; then
+  rm -fr ~/.config/nvim/
+  ## clone NvChad
+  git clone https://github.com/NvChad/starter ~/.config/nvim
+else
 rm -fr ~/.config/nvim/
-## clone NvChad
-git clone https://github.com/NvChad/starter ~/.config/nvim
+  mkdir ~/.config/nvim/
+fi
 ## cleanup current dir and move to root
 rm nvim.appimage
 sudo rm -fr /squashfs-root/
@@ -21,7 +36,6 @@ sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 # Insert wanted configurations for neovim installation here #
 #                                                           #
 #############################################################
-echo -e 'vim.opt.relativenumber = true \n' >> ~/.config/nvim/init.lua
-echo -e 'vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true }) \n' >> ~/.config/nvim/init.lua
-echo -e 'vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true }) \n' >> ~/.config/nvim/init.lua
 
+
+cat further_configs >> ~/.config/nvim/init.lua
